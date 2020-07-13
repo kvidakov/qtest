f215185c9e800fece8d2f118fe580d8b86720c93
Karlo Vidaković <vidakovic.karlo@gmail.com>
2020-07-13 22:30:24 +0200

    remove footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FooterComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
