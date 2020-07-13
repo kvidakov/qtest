a9cc1c25e033b1140c1dcb6950844d0033dec64e
Karlo Vidaković <vidakovic.karlo@gmail.com>
2020-07-13 22:06:50 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'details', loadChildren: () => import('./pages/details/details.module').then(m => m.DetailsModule) },
+  { path: '**', pathMatch: 'full', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
